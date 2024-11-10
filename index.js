546d10c2e25d27ef0b623fc19b3c927f90915a33
ronnmabunga <ronnmabunga@gmail.com>
2024-11-10 16:16:53 +0800

    Add demo API 2
    git submodule add https://github.com/ronnmabunga/blogging-website-app-api-demo ./blogging-website-app-api-demo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const demoApi1 = require("./blogging-website-app-api-demo/index");
+const demoApi2 = require("./expressjs-fs-monolithic-user-api-demo/index");
 require("dotenv").config();
 
 app.use("/demo-api-1", demoApi1);
+app.use("/demo-api-2", demoApi2);
 
 if (require.main === module) {
     app.listen(4000, () => {
