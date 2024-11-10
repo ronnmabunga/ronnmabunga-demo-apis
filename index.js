97cbf86683a410cf9f5c502eed43548dfa1b33f7
ronnmabunga <ronnmabunga@gmail.com>
2024-11-10 16:07:49 +0800

    Add demo API 1
    Command: git submodule add https://github.com/ronnmabunga/blogging-website-app-api-demo ./blogging-website-app-api-demo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const demoApi1 = require("./blogging-website-app-api-demo/index");
 require("dotenv").config();
 
+app.use("/demo-api-1", demoApi1);
 
 if (require.main === module) {
     app.listen(4000, () => {
