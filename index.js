3cc7ce50ad0650c1c33e5e60f923a29be14dc8b2
ronnmabunga <ronnmabunga@gmail.com>
2024-11-10 16:18:35 +0800

    Add demo API 3

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const demoApi1 = require("./blogging-website-app-api-demo/index");
 const demoApi2 = require("./expressjs-fs-monolithic-user-api-demo/index");
+const demoApi3 = require("./expressjs-mongodb-mrc-ecommerce-api-demo/index");
 require("dotenv").config();
 
 app.use("/demo-api-1", demoApi1);
 app.use("/demo-api-2", demoApi2);
+app.use("/demo-api-3", demoApi3);
 
 if (require.main === module) {
     app.listen(4000, () => {
