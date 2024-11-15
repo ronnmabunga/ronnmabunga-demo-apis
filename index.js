7dcf4b75ad133107eb9740de3b1478e6fdd6d550
ronnmabunga <ronnmabunga@gmail.com>
2024-11-16 01:52:24 +0800

    Add demo-api-4 as module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const demoApi1 = require("./expressjs-mongodb-mrc-blogging-api-demo/index");
 const demoApi2 = require("./expressjs-fs-monolithic-user-api-demo/index");
@@ -8,6 +9,16 @@ require("dotenv").config();
 app.use("/demo-api-1", demoApi1);
 app.use("/demo-api-2", demoApi2);
 app.use("/demo-api-3", demoApi3);
+app.use(
+    "/demo-api-4",
+    createProxyMiddleware({
+        target: "http://django:8001",
+        changeOrigin: true,
+        pathRewrite: {
+            "^/demo-api-4": "",
+        },
+    })
+);
 
 if (require.main === module) {
     app.listen(4000, () => {
